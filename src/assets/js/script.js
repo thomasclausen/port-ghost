c45b43af2970f938713ae04cb3e3489f53f8f717
Thomas Clausen <kontakt@thomasclausen.dk>
2015-05-08 23:45:56 +0200

    Getting the data from Facebook

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -15,8 +15,38 @@
 		}
 	}
 
+	function socialCount() {
+		var elements = document.querySelectorAll('.total-share-count');
+
+		for (var i = 0; i < elements.length; i++) {
+			// GET SHARES FROM FACEBOOK
+			var requestFacebook = new XMLHttpRequest();
+			requestFacebook.open('GET', 'https://graph.facebook.com/?id=' + encodeURIComponent(elements[i].getAttribute('data-share-counts')), true);
+			requestFacebook.onreadystatechange = function () {
+				if (this.status === 200 && this.readyState === 4) {
+					var data = JSON.parse(this.responseText);
+					if (data.shares !== undefined) {
+						document.querySelector('.total-share-count[data-share-counts="' + data.id + '"] .total-count').innerHTML = data.shares;
+					}
+				}
+				if (this.status >= 400) {
+					poorMansDebugging('We reached the server, but it returned an error');
+				}
+			};
+
+			requestFacebook.onerror = function () {
+				poorMansDebugging('There was a connection error of some sort');
+			};
+
+			requestFacebook.send();
+		}
+	}
+
 	function init() {
 		// Do something
+		if (classie.hasClass(body, 'post-template')) {
+			socialCount();
+		}
 	}
 
 	documentState = setInterval(function () {
