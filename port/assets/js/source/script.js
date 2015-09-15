ae0dbfbc426e233558e31b8c056eda8dbcb59e60
Thomas Clausen <kontakt@thomasclausen.dk>
2015-09-15 21:45:31 +0200

    Update JS

--- port/assets/js/source/script.js
+++ port/assets/js/source/script.js
@@ -28,18 +28,16 @@
 		}
 	}, 100);
 
-	if (iframesLength !== 0) {
-		for (var i = 0; i < iframesLength; i++) {
-			var iframe = iframes[i],
-				ratio = '';
+	for (var i = 0; i < iframesLength; i++) {
+		var iframe = iframes[i],
+			ratio = '';
 
-			if (iframe.getAttribute('width') > iframe.getAttribute('height')) {
-				ratio = (iframe.getAttribute('height') / iframe.getAttribute('width')) * 100;
-			} else {
-				ratio = (iframe.getAttribute('width') / iframe.getAttribute('height')) * 100;
-			}
-
-			iframe.outerHTML = '<div class="embed-responsive" style="padding-bottom:' + ratio + '%">' + iframe.outerHTML + '</div>';
+		if (iframe.getAttribute('width') > iframe.getAttribute('height')) {
+			ratio = (iframe.getAttribute('height') / iframe.getAttribute('width')) * 100;
+		} else {
+			ratio = (iframe.getAttribute('width') / iframe.getAttribute('height')) * 100;
 		}
+
+		iframe.outerHTML = '<div class="embed-responsive" style="padding-bottom:' + ratio + '%">' + iframe.outerHTML + '</div>';
 	}
 })();
